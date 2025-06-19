89a57cf253f82a646a7ea8f58f94362ae20cc180
Shekhar Rana <119071318+shekharrana007@users.noreply.github.com>
2025-06-19 22:07:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete Middle of Linked List/delete-middle-of-linked-list.cpp
@@ -0,0 +1,37 @@
+/* Link list Node:
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+// Deletes middle of linked list and returns head of the modified list
+class Solution {
+  public:
+    Node* deleteMid(Node* head) {
+        // Your Code Here
+        if(!head || !head->next){
+            delete head;
+            return NULL;
+        }
+        Node* slow=head;
+        Node* fast=head;
+        Node* prev=NULL;
+        while(fast && fast->next){
+            prev=slow;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        prev->next=slow->next;
+        delete slow;
+        return head;
+    }
+};
